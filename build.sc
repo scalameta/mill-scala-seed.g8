2941e2b3bdc09e66d3aa09d2b5a2c830169a260f
ckipp01 <ckipp@pm.me>
2022-08-15 13:49:24 +0200

    refactor: move build over to mill

---
+++ build.sc
@@ -0,0 +1,7 @@
+import $ivy.`io.chris-kipp::mill-giter8::0.2.0`
+
+import io.kipp.mill.giter8.G8Module
+
+object g8 extends G8Module {
+  override def validationTargets = Seq("__.compile", "__.test")
+}
--- src/main/g8/$moduleName$/test/src/example/HelloSuite.scala
+++ src/main/g8/$moduleName$/test/src/example/HelloSuite.scala
@@ -3,7 +3,7 @@ package example
 class HelloSuite extends munit.FunSuite {
   test("numbers") {
     val obtained = 42
-    val expected = 43
+    val expected = 42
     assertEquals(obtained, expected)
   }
 
